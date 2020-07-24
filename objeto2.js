ac6aeead8d0f67d59e10d5de12058b4f1266a7b8
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-07-24 19:58:55 -0300

    adicionei um novo arquivo

---
+++ objeto2.js
@@ -0,0 +1,11 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const cliente = function() {}
+console.log(typeof new cliente)
+
+class produto {} //es 2015 (es6)
+console.log(typeof produto)
+console.log(typeof new produto)
+
+
