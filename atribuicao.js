81e2a1f717e1f8e381554fbab5d0755392ff3a9b
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-07-26 16:07:56 -0300

    adicionei um novo arquivo

---
+++ atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+let b =3
+
+
+b += a // b= b + a 
+console.log(b)
+
+b -= 4 //b= b - 4
+console.log(b)
+
+b *= 2 //b= b * 2
+console.log(b) 
+
+b /= 2 //b= b / 2
+console.log(b)
+
+b %= 2 //b= b % 2 , se o valor for impar ele ira mostrar 1 e de o valor for par ele ira mostra 0.
+console.log(b)
