7b66dbc575da0a4c068d908e35adb07085d02bf4
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-07-31 19:59:48 -0300

    adicionei um novo arquivo

---
+++ aritimeticos.js
@@ -0,0 +1,11 @@
+//exemplos de aritimeticos binarios. pq tem dois operantes
+
+const [a,b,c,d]=[3, 5, 1, 15]
+
+const soma = a + b + c + d 
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 //aqui ele ira ver se o resultado de a/2 é impar ou par se for impar ele ira mostrar o numero 1 e se for par ele ira mostrar o numero 0.
+
+console.log(soma,subtracao,multiplicacao,divisao,modulo)
