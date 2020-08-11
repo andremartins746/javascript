b187b725347f3fcd240c851a6eb03e883d0a8b99
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-08-11 09:07:15 -0300

    adicionei um novo arquivo!

---
+++ estruturas_de_controle/ifElse.js
@@ -0,0 +1,12 @@
+const imprimirresultado = function (nota){
+    if(nota>=7) {
+        console.log('aprovado!')
+    } else{
+        console.log('reprovado!!')
+    }
+}
+
+imprimirresultado(6)
+imprimirresultado(7)
+imprimirresultado(10)
+imprimirresultado('epa!') //cuidado!!! 
