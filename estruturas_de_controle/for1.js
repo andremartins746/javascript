8d205a5b863338aadb57544e1475ea7c70bd5ab8
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2020-09-10 09:10:29 -0300

    primeiro arquivo FOR.

---
+++ estruturas_de_controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1 //declaração; expreção; encremento.
+while (contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador++
+}
+
+for(let i=1;i<=10;i++) { //declaração; expreção; encremento.
+    console.log(`i = ${i}`)
+}
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]//navegação pelo arrey
+for(let i =0; i < notas.length; i++){ //length serve para vc percorrer o arrey.
+    console.log(`notas = ${notas[i]}`)
+    
+}
