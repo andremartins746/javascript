2de7a9ae16c06004cfe5167fd638e287f92b111a
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-01-01 17:05:42 -0300

    arquivos de js

---
+++ parametrosVAriaveis.js
@@ -0,0 +1,15 @@
+function soma(){
+    let soma = 0
+    //o argumentséum arrey nativo do for
+    //que vc pode utilizalo por exemplo em uma soma
+    for(i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+console.log(soma(1.1, 2.2, "teste"))
+console.log(soma("a", "b", "c"))
---
+++ paranPadrao.js
@@ -0,0 +1,34 @@
+//estrategia 1 para gerar valor padrão.
+
+function soma1(a, b, c ){
+    //valor padão
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+//ao passar ovalor zero"0" a estrategia 1 dera um bag poque ele ira asumir o valor false e ira pega comovalor o 1
+console.log(soma1(), soma1(3), soma1(1,2,3), soma1(0,0,0))
+
+//estrategia 2, 3 e 4 para gerar valor padrão.
+
+function soma2(a, b, c){
+    a = a !== undefined ? a : 1
+    b =1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+
+    return a + b + c
+}
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+
+
+//valor padrão de es2015
+
+function soma3(a =1, b=1, c=1){
+    return a + b + c
+}
+
+console.log(soma3(3), soma3(3), soma3(1,2,3), soma3(0,0,0))
