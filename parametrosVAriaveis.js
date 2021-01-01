2ab23364a14baab3f39b6e1acc185f3c87b6cbf7
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-01-01 17:10:20 -0300

    Update parametrosVAriaveis.js

--- parametrosVAriaveis.js
+++ parametrosVAriaveis.js
@@ -1,13 +1,16 @@
 function soma(){
     let soma = 0
     //o argumentséum arrey nativo do for
+    
     //que vc pode utilizalo por exemplo em uma soma
     for(i in arguments){
         soma += arguments[i]
     }
+    
     return soma
 }
 
+//testes
 console.log(soma())
 console.log(soma(1))
 console.log(soma(1.1, 2.2, 3.3))
