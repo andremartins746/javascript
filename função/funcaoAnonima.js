7139cc7c06a1f711a8c8a266b1deb2ab3d76ca13
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-01-10 20:12:37 -0300

    Funções anonimas

---
+++ função/funcaoAnonima.js
@@ -0,0 +1,21 @@
+const soma = function (x, y){
+    return  x + y
+}
+
+const ImprimirResultado = function(a, b, operacao = soma){
+    console.log(a, b)
+}
+
+ImprimirResultado(3, 4)
+ImprimirResultado(3, 4, soma)
+ImprimirResultado(3, 4, function (x, y){
+    return x - y
+})
+ImprimirResultado(3, 4, (x, y) => x + y)
+
+const pessoa = {
+    falar: function () {
+        console.log("Opa")
+    }
+}
+pessoa.falar()
