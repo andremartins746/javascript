e8a96d0e9b7e633b8af27bfe5efc5d5c6b1ccd2c
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-01-26 15:24:37 -0300

    Update arquivo.js

--- objetos/arquivo.js
+++ objetos/arquivo.js
@@ -1,3 +1,8 @@
 const nomes = ['andré', 'odair', 'luana', 'monique', 'davi']
 
+// usando .foreach()
 nomes.foreach((elemento) => console.log(elemento))
+
+// usando .filter()
+const nomes1 = nomes.filter((nome) => nome === 'davi')
+console.log(nomes1)
