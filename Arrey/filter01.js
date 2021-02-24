5eb4fe70d161188d7fc13a681c9899c8b6df6269
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-02-24 13:33:45 -0300

    Add files via upload

---
+++ Arrey/filter01.js
@@ -0,0 +1,12 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome:'ipad pro', preco: 4199, fragil: true },
+    {nome: 'copo de vidro', preco: 12.49, fragil: true},
+    {nome: 'copo de plastico', preco: 18.99, fragil: false}
+]
+
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil))
