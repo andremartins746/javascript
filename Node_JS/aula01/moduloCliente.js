d2ace8847f6a922760f88b0ab389962608202418
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-02-26 14:29:42 -0300

    Add files via upload

---
+++ Node_JS/aula01/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA') // o require() importa um modulo "arquivo" do node-JS
+const  moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
