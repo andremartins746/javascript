ec25ddafee9e20595f0edade0f1f2fd2a5eb5ce8
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-04-14 12:59:04 -0300

    Add files via upload

---
+++ ESNext/promise_2.js
@@ -0,0 +1,28 @@
+/* 
+setTimeout(function () {
+    console.log('Execultando callback...')
+   
+
+    setTimeout(function() {
+        console.log('Execultando callback...')
+
+        setTimeout(function() {
+            console.log('Execultando callback...')
+        }, 2000)
+
+    }, 2000)
+
+}, 2000) */
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('execultando promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(() => esperarPor())
