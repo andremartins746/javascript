aaf9d9dfe0a33a2838d882d8d1263b99a79a0cb4
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-04-14 13:01:13 -0300

    Add files via upload

---
+++ ESNext/asyncAwait.js
@@ -0,0 +1,42 @@
+// com promise...
+const http = require('http')
+const { get } = require('node:http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+
+   return new Promise((resolve, reject) => {
+    http.get(url, res => {
+        let resultado = ''
+
+        res.on('data', dados => {
+            resultado += dados
+        })
+
+        res.on('end', () => {
+          try {
+              resolve(JSON.parse(resultado))
+            } catch(e) {
+                reject(e)
+            }
+
+        })
+    })
+   })
+}
+
+
+// recurso do ES8
+// simplificar o uso de promises...
+
+let obteralunos = async () => {
+    const ta = await getTurma('A')
+    const tb = await getTurma("B")
+    const tc = await getTurma("C")
+
+    return [].concat(ta, tb, tc)
+}// retorna um objeto AsyncFunction
+
+obteralunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then( nomes => console.log(nomes))
