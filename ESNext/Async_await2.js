bbb6ef39a3a9d3a21f84af0fc0515c6658286d11
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-04-15 14:14:20 -0300

    async/ await

---
+++ ESNext/Async_await2.js
@@ -0,0 +1,46 @@
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout( () => resolve(), tempo)
+    })
+}
+
+/* esperarPor(2000)
+    .then(() => console.log('execultando promise 1...'))
+    .then(esperarPor)
+    .then(() => console.log('execultando promise 2...'))
+    .then(esperarPor)
+    .then(() => console.log('execultando promise 3...'))
+
+*/
+
+function retornaValor() {
+    return new Promise(resolve => resolve(10)) 
+}
+
+async function execultar() {
+
+    let valor = await retornaValor() // o await só funciona em uma função que retorna uma promise
+
+    await esperarPor(1500) // ele só vai execultar a proxima linha depois de ter execultado a promise 
+    console.log(`async/await ${valor}...`)
+
+    await esperarPor(1500) // ele espera essa linha ser execultada para partir para a proxiama linha
+    console.log(`async/await ${valor + 1}...`)
+
+    await esperarPor(1500)
+    console.log(`async/await ${valor + 2}...`)
+
+    return valor + 3 
+}
+
+
+async function execultarDeVerdade() {
+   let valor = await execultar()
+   console.log(valor)
+}
+execultarDeVerdade()
+
+
+
+
--- ESNext/promise_5.js
+++ ESNext/promise_5.js
@@ -1,11 +1,19 @@
+// tratamento de Erros em promises
+
 function funcionarOuNao(valor, chanceErr) {
     return new Promise((resolve, reject) => {
+    try{
+       
         if(Math.random() < chanceErr) {
             // aqui estamoos gerando um Erro
             reject('Ocorreu um Erro')
         } else {
             resolve(valor)
         }
+
+    } catch(e){
+        reject(e)
+    }
     })
 }
 
